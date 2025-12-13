d93f44b45ac6d2b129b74ca75fac1259a26c73e0
mpr <marinpericradan1@gmail.com>
2025-12-13 16:54:38 +0100

    basic url from html retrieval logic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello, World!')
+import { argv } from "process";
+import { getHTML } from "./crawl"
+
+
+async function main() {
+  if (argv.length < 3) {
+    console.error(`function main() - not enough arguments:\n${argv.length}`)
+    process.exit(1)
+  }
+
+  if (argv.length > 3) {
+    console.error(`function main() - too many arguments:\n${argv.length}`)
+    process.exit(1)
+  }
+
+  const BASE_URL = argv[2]
+  console.log(`Crawler starting work on ${BASE_URL}`)
+  await getHTML(BASE_URL)
+}
+
+await main()
+
