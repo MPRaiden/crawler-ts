4c03ede4fd4e61b8a542772b4aaa2746d6b35681
mpr <marinpericradan1@gmail.com>
2026-01-17 15:53:44 +0100

    chore: added max concurency as a input param to the script

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,10 @@ async function main() {
     process.exit(1)
   }
 
-  if (argv.length > 3) {
-    console.error(`Too many arguments, You provided: ${argv.length}, please provide 3.`)
-    process.exit(1)
-  }
-
   const BASE_URL = argv[2]
+  const MAX_CONCURENCY = Number(argv[3])
   console.log(`Crawler starting work on ${BASE_URL}`)
-  const pages = await crawlSiteAsync(BASE_URL, 3)
+  const pages = await crawlSiteAsync(BASE_URL, MAX_CONCURENCY)
   console.log(pages)
 }
 
